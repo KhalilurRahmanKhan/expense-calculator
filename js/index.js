33c127a6e4145b0776498a680dae131863cce258
KhalilurRahmanKhan <khanjahid373@gmail.com>
2022-02-17 11:07:02 +0600

    functionality add

--- js/index.js
+++ js/index.js
@@ -1,17 +1,64 @@
-document.getElementById("calculate-expenses").addEventListener("click",function(){
-    let salary = document.getElementById("salary").value;
-    console.log(typeof salary);
-    salary = handleError(salary);
-    console.log(typeof salary);
-});
 function handleError(value){
-    if(typeof value != "number"){
-        document.getElementById("expense-err").innerText="Value must be a number";
-        return null;
+    document.getElementById("expense-err").innerText="";
+
+    
+    if(isNaN(value)){
+        return 0;
     }
     if(value < 0){
         document.getElementById("expense-err").innerText="Value cannot be a negetive number";
-        return null;
+        return 0;
     }
+    
     return value;
 }
+
+function calculate(){
+    let salary = handleError(parseFloat(document.getElementById("salary").value));
+ 
+
+    let food = handleError(parseFloat(document.getElementById("food").value));
+    let rent = handleError(parseFloat(document.getElementById("rent").value));
+    let clothes = handleError(parseFloat(document.getElementById("clothes").value));
+
+    let save = handleError(parseFloat(document.getElementById("save").value));
+
+
+    let totalExpenses = food+rent+clothes;
+    let balance = salary-totalExpenses;
+
+    if(totalExpenses > salary){
+        document.getElementById("expense-err").innerText="Expenses exceed the balance";
+
+    }
+
+    else{
+        
+    document.getElementById("total-expenses").innerText=totalExpenses;
+    document.getElementById("balance").innerText=balance;
+    }
+
+    let savingAmount = salary*(save/100);
+    let remainingBalance = balance-savingAmount;
+
+    
+    document.getElementById("saving-amount").innerText=savingAmount;
+    document.getElementById("remaining-balance").innerText=remainingBalance;
+}
+
+
+document.getElementById("calculate-expenses").addEventListener("click",function(){
+
+    calculate();
+  
+});
+
+document.getElementById("calculate-saving").addEventListener("click",function(){
+
+
+   calculate();
+
+  
+});
+
+
